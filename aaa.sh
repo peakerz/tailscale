b3b2cb7475eab27f77e2db3899c30656bf73b56f
peakerz <133618876+peakerz@users.noreply.github.com>
2023-05-15 14:26:48 +0800

    Update aaa.sh

--- aaa.sh
+++ aaa.sh
@@ -2,5 +2,4 @@
 # for mx linux wild flower
 curl -fsSL https://tailscale.com/install.sh | sh
 
-tailscale up --login-server=http://176.97.70.199:8080 --exit-node-allow-lan-access
-tailscale up --login-server=http://176.97.70.199:8080 --reset
+
